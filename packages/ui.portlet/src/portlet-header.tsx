9ec2060232db88e27697691e21370707f51abe0c
lamlv <lam.luongvan2305@gmail.com>
2021-07-31 11:46:03 +0700

    update div type for portlet

--- packages/ui.portlet/src/portlet-header.tsx
+++ packages/ui.portlet/src/portlet-header.tsx
@@ -1,16 +1,26 @@
 import { forwardRef } from 'react';
 import { PortletHeaderProps } from './portlet.type';
 
-export const PortletHeader = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement> & PortletHeaderProps>((props, ref) => {
-  const { title, toolbar, subTitle, className, children, ...rest } = props;
+export const PortletHeader = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement> & PortletHeaderProps>(
+  (props, ref) => {
+    const { title, toolbar, subTitle, className, children, ...rest } = props;
 
-  return (
-    <div className={`flex items-center justify-between rounded bg-white px-7 py-4 border-b ${className}`} ref={ref} {...rest}>
-      <div>
-        <span className="uppercase font-bold">{title}</span>
-        <span className="uppercase">{subTitle}</span>
+    return (
+      <div
+        className={`flex items-center justify-between rounded bg-white px-7 py-4 border-b ${className}`}
+        ref={ref}
+        {...rest}
+      >
+        {children ?? (
+          <>
+            <div>
+              <span className="uppercase font-bold">{title}</span>
+              <span className="uppercase">{subTitle}</span>
+            </div>
+            <div>{toolbar}</div>
+          </>
+        )}
       </div>
-      <div>{toolbar}</div>
-    </div>
-  );
-});
+    );
+  }
+);
--- packages/ui.portlet/src/portlet.type.ts
+++ packages/ui.portlet/src/portlet.type.ts
@@ -1,5 +1,5 @@
 export interface PortletHeaderProps {
-  title?: string | React.ReactElement;
-  toolbar?: React.ReactElement;
-  subTitle?: string | React.ReactElement;
+  title?: string | JSX.Element;
+  toolbar?: JSX.Element;
+  subTitle?: string | JSX.Element;
 }
